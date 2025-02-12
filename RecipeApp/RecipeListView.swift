e907d4174d7c79e701cb21ebac0d61c7367a4aab
srivalli93 <srivalli.kanchibotla@okstate.edu>
2025-02-11 22:52:16 -0600

    Code clean up

--- RecipeApp/RecipeListView.swift
+++ RecipeApp/RecipeListView.swift
@@ -10,8 +10,6 @@ import SwiftUI
 struct RecipeListView: View {
     
     @StateObject private var fetchService = RecipeService()
-    @State private var selectedCuisine: String = "All"
-    @State private var searchText: String = ""
     
     var body: some View {
         NavigationView {
@@ -32,30 +30,12 @@ struct RecipeListView: View {
                     }
                     
                     List(fetchService.selectedRecipes, id: \.uuid) { recipe in
-                        HStack {
-                            AsyncImageView(url: recipe.photo_url_small ?? "")
-                                .frame(width: 100, height: 100)
-                                .clipShape(.rect(cornerRadius: 20))
-                                .overlay(RoundedRectangle(cornerRadius: 20.0).stroke(Color.clear, lineWidth: 2))
-                            
-                            
-                            VStack (alignment: .leading, spacing: 3) {
-                                Text(recipe.name)
-                                    .fontWeight(.semibold)
-                                    .lineLimit(1)
-                                    .minimumScaleFactor(0.8)
-                                Text(recipe.cuisine)
-                                    .font(.subheadline)
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .padding(.vertical, 8)
-                        .transition(.slide)
+                        RecipeRow(recipe: recipe)
                     }
                     .listStyle(PlainListStyle())
                     .animation(.easeIn, value: fetchService.recipes)
                     .navigationTitle("Recipes")
-                    .searchable(text: $searchText)
+                    .searchable(text: $fetchService.searchText)
                     .task {
                         await fetchService.fetchRecipes()
                     }
@@ -63,24 +43,51 @@ struct RecipeListView: View {
             }
         }
     }
+}
+
+struct CuisineButton: View {
     
-    struct CuisineButton: View {
-        
-        var cuisine: String
-        var isSelected: Bool
-        var action: () -> Void
-        
-        var body: some View {
-            Button(action: action) {
-                Text(cuisine)
-                    .padding()
-                    .fontWeight(isSelected ? .bold : .regular)
-                    .background(isSelected ? Color.orange.opacity(0.2) : .clear)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .overlay(RoundedRectangle(cornerRadius: 20).stroke(isSelected ? Color.orange : Color.clear, lineWidth: 1.5))
-            }
-            .buttonStyle(PlainButtonStyle())
+    var cuisine: String
+    var isSelected: Bool
+    var action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(cuisine)
+                .padding()
+                .fontWeight(isSelected ? .bold : .regular)
+                .background(isSelected ? Color.orange.opacity(0.2) : .clear)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .overlay(RoundedRectangle(cornerRadius: 20).stroke(isSelected ? Color.orange : Color.clear, lineWidth: 1.5))
         }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
+struct RecipeRow: View {
+    
+    var recipe: Recipe
+    
+    var body: some View {
+        HStack {
+            AsyncImageView(url: recipe.photo_url_small ?? "")
+                .frame(width: 100, height: 100)
+                .clipShape(.rect(cornerRadius: 20))
+                .overlay(RoundedRectangle(cornerRadius: 20.0).stroke(Color.clear, lineWidth: 2))
+            
+            
+            VStack (alignment: .leading, spacing: 3) {
+                Text(recipe.name)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.8)
+                Text(recipe.cuisine)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+        .padding(.vertical, 8)
+        .transition(.slide)
     }
 }
 
